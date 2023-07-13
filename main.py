66381b5e703afa8bf25835f01da86a4fa1fa7fc3
Aydin Ali <aydina2@illinois.edu>
2023-07-12 21:36:52 -0700

    Fixed some local Python interpreter issues and added small bot-testing hello function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import discord
+
+with open("token.txt", "r") as token_file:
+  TOKEN = token_file.read()
+
+intents = discord.Intents.all()
+
+from discord.ext import commands
+prefix = "!"
+bot = commands.Bot(command_prefix = prefix, intents = intents)
+
+@bot.command()
+async def hello(ctx):
+  await ctx.send("Hello!")
+
+bot.run(TOKEN)
